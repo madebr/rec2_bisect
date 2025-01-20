34494f0b236674b261ca22b91f102381c0727c23
Anonymous Maarten <anonymous.maarten@gmail.com>
2025-01-20 23:44:23 +0100

    Add start.bat and download.bat to archive

--- create_package.py
+++ create_package.py
@@ -60,5 +60,7 @@ with zipfile.ZipFile(ZIP_PACKAGE_PATH, "w") as zf:
         rel_path = Path(root).relative_to(PROJECT_ROOT)
         for filename in filenames:
             zf.write(str(Path(root) / filename), arcname=str(rel_path / filename))
+        zf.write(PROJECT_ROOT / "start.bat", arcname="start.bat")
+        zf.write(PROJECT_ROOT / "download.bat", arcname="download.bat")
 
 print(f"[X] Created {ZIP_PACKAGE_PATH}")
